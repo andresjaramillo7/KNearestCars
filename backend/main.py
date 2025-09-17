0a02a9d6e0019e94032a41538015c28fd1f33f35
a01029143 <a010291443@tec.mx>
2025-09-17 17:12:30 -0600

    Updated all ML models

--- backend/main.py
+++ backend/main.py
@@ -5,13 +5,22 @@ Ricardo Calvo Pérez - A01028889
 
 Main script para ejecutar todos los modelos de Machine Learning
 """
-
+# Importar los modelos
 from MachineLearningModels.KNN.knn import main_knn
 from MachineLearningModels.LogisticRegresion.logistic_regresion import main_logistic
 from MachineLearningModels.NaiveBayes.naive_bayes import main_naive_bayes
 
 from Data.data_preparation import training, trainingLabels, test, testLabels
 
-main_knn(training, trainingLabels, test, testLabels)
+print("\n============================== KNN ==============================")
+# Llamar modelo KNN
+k = 5
+main_knn(training, trainingLabels, test, testLabels, k)
+
+print("\n============================== Regresión Logística ==============================")
+# Llamar modelo Regresión Logística
 main_logistic(training, trainingLabels, test, testLabels)
-# main_naive_bayes()
+
+print("\n============================== Naive Bayes ==============================")
+# Llamar modelo Naive Bayes
+main_naive_bayes(training, trainingLabels, test, testLabels)
