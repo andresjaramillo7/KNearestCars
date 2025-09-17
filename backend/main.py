056c26c7e1c6b0ecb906de72fb25ae5176f9311b
andresjaramillo7 <a01029079@tec.mx>
2025-09-17 17:12:03 -0600

    feat: training and test in load_pattern and use it in main

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,10 @@ from MachineLearningModels.KNN.knn import main_knn
 from MachineLearningModels.LogisticRegresion.logistic_regresion import main_logistic
 from MachineLearningModels.NaiveBayes.naive_bayes import main_naive_bayes
 
-from Data.data_preparation import training, trainingLabels, test, testLabels
+from Data.data_preparation import load_dataset
 
-main_knn(training, trainingLabels, test, testLabels)
+training, trainingLabels, test, testLabels = load_dataset("Data/WinLoseDataset.csv")
+
+# main_knn(training, trainingLabels, test, testLabels)
 main_logistic(training, trainingLabels, test, testLabels)
 # main_naive_bayes()
